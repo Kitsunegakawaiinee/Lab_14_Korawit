a06ab773b83529dbeac1f47bc26215c268e280f2
SKOT <Korawit_pi@cmu.ac.th>
2023-01-13 17:30:24 +0700

    ข้อ 1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,79 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+double findAmean(const double a[], int N)
+{
+    double sum =0;
+    for (int i = 0; i < N; i++) sum+= a[i];
+    return sum/N;
+}
+
+double findx(const double a[], int N, double mean, double &sum)
+{
+    if (N >0) findx(a,N-1,mean,sum);
+
+    sum += pow((a[N]-mean), 2);
+
+    return sum;
+
+}
+
+double StandardDeviation(double mean, int N, const double A[])
+{
+    double sum = 0;
+    return sqrt((findx(A,N-1,mean,sum)/N));
+}
+
+double Geo(const double a[], int N)
+{
+    double sum = a[0];
+
+    for(int i = 1; i < N; i++)
+    {
+        sum *= a[i];
+    }
+
+    return pow(sum, 1.0/N);
+
+}
+
+double Harmonic_Mean(const double a[], int n)
+{
+    double sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum +=(1/a[i]);
+    }
+    return n/sum;
+    
+}
+
+
+void stat(const double a[], int N, double b[])
+{
+    b[0] = findAmean(a, N);
+    b[1] = StandardDeviation(b[0], N, a);
+    b[2] = Geo(a, N);
+    b[3] = Harmonic_Mean(a,N);
+
+    double Max,Min;
+    bool Setmax = false ,Setmin = false; 
+
+    for (int i = 0; i<N; i++)
+    {
+        if (Setmax == false) Max = a[0];
+        if (Setmin == false) Min = a[0];
+
+        for (int j = 0; j < N; j++)
+        {
+            if (Max < a[j]) Max = a[j];
+            if (Min > a[j]) Min = a[j];
+        }
+
+    }
+
+    b[4] = Max;
+    b[5] = Min;
+}
