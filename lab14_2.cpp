83f77ee73b8a3ccdbe015b79df5b103268cbfbef
SKOT <skotskip@gmail.com>
2023-01-13 21:47:54 +0700

    ข้อ 2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,36 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool AR [][M], int brush, int X, int Y)
+{
+    bool a;
+    for(int i = 0; i < 30; i++)
+    {
+        for(int j = 0; j < 70;j++)
+        {
+            a = sqrt(pow((i-X),2) + pow((j-Y),2)) <= brush-1;
+            if (a) AR[i][j] = 1;
+        }
+    }
+}
+
+void showImage(const bool AR [][M])
+{
+    cout << "------------------------------------------------------------------------\n";
+
+    for (int i = 0; i < 30; i++)
+    {
+        cout << "|";
+
+        for(int j = 0; j< 70; j++)
+        {
+            if(AR[i][j] == 0) cout << " ";
+            else cout << "*";
+        }
+
+        cout << "|" <<endl;
+    }
+
+    cout << "------------------------------------------------------------------------\n";
+}
